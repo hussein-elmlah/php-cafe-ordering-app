737736ff8e0d8fbd9eb8d0970f13956a89058013
hussein-elmlah <husseinelmlah11@gmail.com>
2024-04-25 12:00:57 +0200

    Feat: add footer

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 </head>
-<body>
+<body class="">
     <?php include './includes/navbar.php'; ?>
 
-    <div class="container">
+    <div class="container" style="min-height: 78vh;">
         <h1>Welcome to Our Cafe!</h1>
         <p>Explore our menu and manage orders using the links above.</p>
     </div>
+
+    <?php include './includes/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
