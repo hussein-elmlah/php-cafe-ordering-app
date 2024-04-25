23a67ef813808a101faf846fec7906f47b5dc2ed
hussein-elmlah <husseinelmlah11@gmail.com>
2024-04-25 11:27:40 +0200

    Feat: add index.php file as entery point and create navbar

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Our Cafe!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<body>
+    <?php include './includes/navbar.php'; ?>
+
+    <div class="container">
+        <h1>Welcome to Our Cafe!</h1>
+        <p>Explore our menu and manage orders using the links above.</p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
