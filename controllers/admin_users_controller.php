0b3567d612faedf527f2b5b50f8773802a604593
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-04-27 00:38:37 +0300

    feat: update validation is working

--- controllers/admin_users_controller.php
+++ controllers/admin_users_controller.php
@@ -3,7 +3,6 @@ session_start();
 // include (realpath(dirname(__FILE__)."config/db_info.php"));
 require_once 'vendor/autoload.php';
 use Respect\Validation\Validator as DataValidation;
-use Respect\Validation\Exceptions\NestedValidationException;
 use Respect\Validation\Exceptions\ValidationException;
 require_once 'config/db_info.php';
 require_once 'db/db_class.php';
@@ -22,19 +21,21 @@ include 'includes/pagination.php';
 //     }
 // }
 
-class AdminUserController {
+class AdminUserController
+{
     private $db;
 
-    public function __construct (){
+    public function __construct()
+    {
         $this->db = Database::getInstance();
         $this->db->connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
     }
-    public function addAdminUser() {
-        
+    public function addAdminUser()
+    {
+
         $profile = $_FILES['profile']['tmp_name'];
-        $isEmail = DataValidation::email(); 
-        $isImage = DataValidation::image(); 
+        $isEmail = DataValidation::email();
+        $isImage = DataValidation::image();
 
         try {
             DataValidation::stringType()->length(8, null)->check($_POST['password']);
@@ -42,12 +43,16 @@ class AdminUserController {
             DataValidation::keyValue('password_confirmation', 'equals', 'password')->check($_POST);
             $isEmail->check($_POST['email']);
             $isImage->check($profile);
-            
+
             $_POST['profile'] = file_get_contents($profile);
-            $this->db->insert("users", "name,email,password,room,ext,profile",
-                             ":name,:email,:password,:room,:ext,:profile", $_POST);
+            $this->db->insert(
+                "users",
+                "name,email,password,room,ext,profile",
+                ":name,:email,:password,:room,:ext,:profile",
+                $_POST
+            );
             $_SESSION['msg'] = "User is added successfully";
-        } catch(ValidationException $exception) {
+        } catch (ValidationException $exception) {
             $_SESSION['error'] = $exception->getMessage();
         }
         echo '<script> 
@@ -55,16 +60,18 @@ class AdminUserController {
               </script>';
     }
 
-    public function viewAddAdminUser() {        
+    public function viewAddAdminUser()
+    {
         $roomQuery = "SELECT * FROM rooms";
         $rooms = $this->db->customQuery($roomQuery);
         include 'views/admin/admin_add_user_view.php';
     }
 
-    public function displayAdminUsers() {
+    public function displayAdminUsers()
+    {
 
         $current_page = 1;
-        $total_pages = 2; 
+        $total_pages = 2;
 
         $base_query = "SELECT * FROM users";
 
@@ -75,7 +82,7 @@ class AdminUserController {
             'search' => isset($_GET['search']) ? $_GET['search'] : null,
         ];
         $search_fields = ['name', 'name'];
-    
+
         $result = $this->db->paramsQuery($base_query, $params, $search_fields);
 
         $adminUsers = $result['data'];
@@ -90,7 +97,8 @@ class AdminUserController {
         Pagination($current_page, $total_pages);
     }
 
-    public function editAdminUser($userId) {
+    public function editAdminUser($userId)
+    {
         $roomQuery = "SELECT * FROM rooms";
         $rooms = $this->db->customQuery($roomQuery);
         $userData = $this->db->customQuery("SELECT * FROM users WHERE id=$userId");
@@ -98,40 +106,59 @@ class AdminUserController {
         return $userData;
     }
 
-    public function updateAdminUser($userId, $user_data) {
+    public function updateAdminUser($userId, $user_data)
+    {
 
-        // if a new image is uploaded.
-        $_POST['room'] = intval($_POST['room']);
-        
-        if (($_FILES['profile']['tmp_name'])) {
-            $profile = file_get_contents($_FILES['profile']['tmp_name']);
-            $_POST['profile'] = $profile;
+        $profile = $_FILES['profile']['tmp_name'];
+        $isEmail = DataValidation::email();
+        $isImage = DataValidation::image();
+
+        try {
+            DataValidation::stringType()->length(8, null)->check($_POST['password']);
+            DataValidation::stringType()->length(8, null)->check($_POST['password_confirmation']);
+            DataValidation::keyValue('password_confirmation', 'equals', 'password')->check($_POST);
+            $isEmail->check($_POST['email']);
+            
+            
+            // if a new image is uploaded.
+            $_POST['room'] = intval($_POST['room']);
+            
+            if ($profile) {
+                $_POST['profile'] = file_get_contents($profile);
+                $isImage->check($profile);
+            }
+
+            $db_fields = array_keys($user_data[0]);
+            array_shift($db_fields);
+            $fields = '';
+            $fields_to_be_updated = '';
+            $values_to_be_updated = [];
+
+            foreach ($db_fields as $field) {
+                if (isset($_POST[$field]) && $user_data[0][$field] !== $_POST[$field]) {
+                    $fields .= "$field=:$field,";
+                    $fields_to_be_updated .= ":$field,";
+                    $values_to_be_updated[] = $_POST[$field];
+                }
+            }
+            $fields = rtrim($fields, ",");
+            $fields_to_be_updated = rtrim($fields_to_be_updated, ",");
+            if (count($values_to_be_updated) > 0) {
+                $_SESSION['msg'] = "User is updated successfully";
+                $this->db->update("users", $userId, $fields, $fields_to_be_updated, $values_to_be_updated);
+            }
+
+        } catch (ValidationException $exception) {
+            $_SESSION['error'] = $exception->getMessage();
         }
 
-        $db_fields = array_keys($user_data[0]);
-        array_shift($db_fields);
-        $fields = '';
-        $fields_to_be_updated = '';
-        $values_to_be_updated = [];
-        
-        foreach ($db_fields as $field) {
-            if ( isset($_POST[$field]) && $user_data[0][$field] !== $_POST[$field]) {
-                $fields .= "$field=:$field,";
-                $fields_to_be_updated .= ":$field,";
-                $values_to_be_updated[] = $_POST[$field];
-            }            
-        }
-        $fields = rtrim($fields, ","); 
-        $fields_to_be_updated = rtrim($fields_to_be_updated, ","); 
-        if (count($values_to_be_updated) > 0) {
-            $this->db->update("users", $userId, $fields, $fields_to_be_updated, $values_to_be_updated);
-        }
         echo '<script>
                 window.location.href = window.location.pathname + "?view=admin-users";
               </script>';
     }
 
-    public function deleteAdminUser($userId) {
+    public function deleteAdminUser($userId)
+    {
         $delete = $this->db->delete("users", $userId);
         $this->displayAdminUsers();
     }
@@ -180,5 +207,3 @@ switch ($action) {
         $adminUserController->displayAdminUsers();
         break;
 }
-
-?>
