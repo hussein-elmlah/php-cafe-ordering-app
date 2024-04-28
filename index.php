f6ee7443129fddbc8f48350421298097146a5fb5
Steven Adel <stevenadel.dev@gmail.com>
2024-04-28 22:19:15 +0300

    Tweak index.html styling

--- index.php
+++ index.php
@@ -15,20 +15,19 @@ error_reporting(E_ALL);
     <title>Welcome to Our Cafe!</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    <style>
+        h1, h2, h3, h4, h5, h6, p {
+           margin: 0;
+           padding: 0;
+        }
+    </style>
 </head>
-<body class="">
-    <!-- navbar -->
-    <?php include './includes/navbar.php'; ?>
-    <!-- router container -->
-    <div class="container" style="min-height: 78vh;">
-
-        <!-- router -->
+<body>
+    <?php include 'includes/navbar.php'; ?>
+    <div class="container-fluid px-1" style="min-height: 78vh">
         <?php include 'router.php'; ?>
-
     </div>
-    <!-- footer -->
-    <?php include './includes/footer.php'; ?>
+    <?php include 'includes/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
