c7da7b02e6fc6ef02b5c341e45db8bf7dca6941b
fatmaelhot <fatmaelhot289@gmail.com>
2024-04-30 06:08:26 +0300

    handle update product view

--- views/admin/add_product.php
+++ views/admin/add_product.php
@@ -1,8 +1,11 @@
+
 <?php if ($loggedUser['isAdmin']): ?>
+    <?php 
+$price = isset($_POST["price"]) ? $_POST["price"] : ''; 
+?>
 <div class="container mt-5">
-    <h1>Add Product</h1>
+    <h1>Add Product</h1><br>
     <form action="" method="post" enctype="multipart/form-data">
-        <!-- Form fields for adding a product -->
         <div class="form-group">
             <label for="name">Product Name:</label>
             <input type="text" name="name" id="name" class="form-control" required>
@@ -36,10 +39,45 @@
         <span style="color: green;"><?php echo $success_message; ?></span>
     </form>
 </div>
-<?php endif; ?>
+
+<!-- update_product_view -->
+<div class="container mt-5">
+    <h1>update Product</h1><br>
+<form action="" method="post" enctype="multipart/form-data">
+<div class="mb-3">
+        <label for="id" class="product_id">Product Id:</label>
+    <input type="number" name="product_id" value="<?php echo $id; ?>"required>
+    </div>
+    <div class="mb-3">
+        <label for="name" class="form-label">Product Name:</label>
+        <input type="text" name="product_name" value="<?php echo $name; ?>" class="form-control" required>
+    </div>
+    <div class="mb-3">
+        <label for="price" class="form-label">Product Price:</label>
+        <input type="number" name="product_price" value="<?php echo $price; ?>" class="form-control" required>
+    </div>
+    <div class="mb-3">
+        <label for="image" class="form-label">Product Image:</label>
+        <input type="file" name="image" id="image" class="form-control" accept="image/*" required>
+    </div>
+    <div class="form-group">
+            <label for="category">Category:</label>
+            <select name="category" id="category" class="form-control" required>
+                <?php foreach ($categories as $category): ?>
+                    <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+    <button type="submit" class="btn btn-primary">Update Product</button>
+</form>
+    </div>
+    <?php endif; ?>
+
+
+
 
 <div class="container mt-5">
-    <h1>List of Products</h1>
+    <h1>List of Products</h1><br>
     <div class="row row-cols-1 row-cols-md-3 g-4">
         <?php foreach ($products as $product): ?>
             <div class="col">
@@ -49,7 +87,13 @@
                         <h5 class="card-title"><?php echo $product['name']; ?></h5>
                         <p class="card-text"><?php echo $product['description']; ?></p>
                         <p class="card-text">Price: <?php echo $product['price']; ?> <span>EG</span></p>
-                        <!-- Add any additional content here -->
+                        <div class="mt-auto">
+                        <button class="edit-product-button btn btn-primary" data-product-id="<?php echo $product['id']; ?>">ُEdit</button>
+                            <button class="btn btn-danger btn-delete" data-product-id="<?php echo $product['id']; ?>">Delete</button>
+                            <?php if (!$loggedUser['isAdmin']): ?>
+                            <button class="btn btn-primary " >Add To Cart</button>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -58,4 +102,25 @@
 </div>
 <br>
 <br>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.min.js"></script>
+
+
+
+   
+<script>
+    var editButtons = document.querySelectorAll('.edit-product-button');
+
+    editButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+        
+            var productId = button.getAttribute('data-product-id');
+        
+            window.location.href = '?view=admin-products&product_id=' + productId;
+        });
+    });
+</script>
 
