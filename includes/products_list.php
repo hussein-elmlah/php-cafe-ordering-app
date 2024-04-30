3e5d5c0fb6f7447b558e366f89f2552e487704f8
Steven Adel <stevenadel.dev@gmail.com>
2024-04-30 22:41:28 +0300

    Show product image dynamically in home

--- includes/products_list.php
+++ includes/products_list.php
@@ -3,7 +3,7 @@
 <form method="post" action="" class="d-flex flex-wrap gap-3">
     <?php foreach ($products as $product) : ?>
         <div class="card" style="width: 18rem; cursor: pointer;">
-            <img src="public/images/drink.jpg" class="card-img-top" alt="Drink">
+            <img src="<?php echo $product['image']; ?>" class="card-img-top" alt="Drink">
             <div class="card-body text-center">
                 <h5 class="card-title"><?php echo $product['name']; ?></h5>
                 <p class="card-text"><?php echo $product['price'] . ' EGP'; ?></p>
